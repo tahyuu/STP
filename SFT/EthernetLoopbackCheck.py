ef28b05a50e1a26c51a9b64ccd4695bf1ebf574a
root <root@sacmvl841.americas.ad.flextronics.com>
2018-03-23 02:33:01 -0400

    updaete EthernetLoopback test program

--- SFT/EthernetLoopbackCheck.py
+++ SFT/EthernetLoopbackCheck.py
@@ -20,7 +20,7 @@ class EthernetLoopbackCheck(TestBase):
 	self.port1=""
 	self.port2=""
 	#self.busIds=["0000:06:05.0","0000:05:00.0"]
-	self.busIds=["0000:00:08.0","0000:00:09.0"]
+	self.busIds=["0000:00:09.0","0000:00:0a.0"]
 	self.device="ixgbe"
 	self.fw="0x800009fa"
     def Start(self):
