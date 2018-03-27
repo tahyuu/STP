966550e877b2bf2b52e105221044f77d33a229d8
root <root@localhost.localdomain>
2018-03-27 10:18:29 -0400

    update the BmcCheck.py

--- SFT/BmcCheck.py
+++ SFT/BmcCheck.py
@@ -9,10 +9,15 @@ class BmcCheck(TestBase):
 	self.pattern = self.BmcCheck
 	self.p = re.compile(self.pattern)
 	self.version = config.Get('BMC_Version')
+	self.bmc_username="admin"
+	self.bmc_password="admin"
+	self.bmc_ipaddr=""
+	self.bmc_command_header="ipmitool -I lanplus -H 192.168.4.193 -U admin -P admin"
     def Start(self):
 	self.log.Print(BmcCheck.section_str)
 	try:
-            self.CheckVersion()
+	    self.GetBmcIpaddr()
+            #self.CheckVersion()
 	except Error, error:
             errCode, errMsg = error
             self.log.Print('TestEnd => ErrorCode=%s: %s' % \
@@ -21,6 +26,18 @@ class BmcCheck(TestBase):
         else:
             self.log.Print("Tester => OK: BMC Version Check")
             return 'PASS'
+    def GetBmcIpaddr(self):
+	#self.comm.SendReturn('ipmitool lan print 0')
+#	line = self.comm.RecvTerminatedBy()
+	self.BmcIpPat= \
+		r'(?P<bmc_ip>\d+\.\d+\.\d+\.\d+)'
+	self.p = re.compile(self.BmcIpPat)
+	line ="1.2.3.4"
+	m = self.p.search(line)
+	print m.group("bmc_ip")
+    def CheckUserSummary(self):
+	self.comm.SendReturn('ipmitool mc info')
+	line = self.comm.RecvTerminatedBy()
 
     def CheckVersion(self):
 	#self.comm.SendReturn('service ipmi start')
@@ -52,7 +69,7 @@ if __name__ == '__main__':
 	parse.error("wrong number of arguments")
 	sys.exit(1)
     home_dir = os.environ['FT']
-    config = Configure(home_dir + '/BFTConfig.txt')
+    config = Configure(home_dir + '/SFTConfig.txt')
     config.Put('HOME_DIR',home_dir)
 
     serial_port = config.Get('port')
